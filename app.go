873f466e7dc919aecbd62eecb313c531429330ca
borogarr <kgonzalez99@ufl.edu>
2017-03-05 23:33:53 -0500

    Added more tests to main_test.go. Debugged postgresql connection errors.

--- app.go
+++ app.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
 
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
@@ -15,7 +17,15 @@ type App struct {
 
 //Initialize will assign the database values to the App variable referenced.
 func (a *App) Initialize(user, password, dbname string) {
+	connectionString := fmt.Sprintf("user=%s password=%s dbname=%s port=5433 sslmode=disable", user, password, dbname)
 
+	var err error
+	a.DB, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	a.Router = mux.NewRouter()
 }
 
 //Run will start the App reference on the port passed in.
